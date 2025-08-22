06a41400abe672f713e93ebf9b38f66cab2c8a3f
saliniyan <saliniyanp02@gmail.com>
2025-08-22 22:54:35 +0530

    modified hosted path

--- frontend/src/modules/core/api.js
+++ frontend/src/modules/core/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://kristallball.onrender.com'
 
 const instance = axios.create({
   baseURL: BASE_URL,
